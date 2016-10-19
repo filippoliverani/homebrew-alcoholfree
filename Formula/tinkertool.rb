40f70a99e45ce4f77e504c49d80ce14dba3dc189
Filippo Liverani <fili_live@yahoo.it>
2016-10-19 17:29:27 +0200

    initial import

---
+++ Formula/tinkertool.rb
@@ -0,0 +1,10 @@
+cask 'tinkertool' do
+  version '6.0'
+  sha256 '5881ac268e91e526bc07b1e43c4e9f8322218465af9456a8df729eaecbdb7960'
+
+  url 'http://download2us.softpedia.com/dl/ab25ad9958d42ad7896d3d24d1415318/580790d5/400005270/mac/Utilities/TinkerTool.dmg'
+  homepage 'http://www.bresink.com/osx/TinkerTool.html'
+  license :gratis
+
+  app 'TinkerTool.app'
+end
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# homebrew-alcoholfree
+# Alcoholfree
 Personal Homebrew tap
+
+## Usage
+Add tap
+```
+brew tap filippo-liverani/alcoholfree
+```
+
+Install formula
+```
+brew install <formula>
+```
