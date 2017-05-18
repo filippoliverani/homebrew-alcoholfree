b2d59f3c363697b940b5f98b22b492e78a2fadb0
Filippo Liverani <filippo-liverani@users.noreply.github.com>
2017-05-18 20:46:37 +0200

    Update Anodizer cask to 1.1.0

--- Casks/anodizer.rb
+++ Casks/anodizer.rb
@@ -1,6 +1,6 @@
 cask 'anodizer' do
-  version '1.0.0'
-  sha256 '702a06e1b85c75b2954d02123a6fc72cfb66ae36c7783c5b100d7fcab198475f'
+  version '1.1.0'
+  sha256 '9dc9aefd94192ef23515b7d79e1ba2c19daa0a03e75ddb511224940d41fc8f24'
 
   url "https://github.com/filippo-liverani/anodizer/releases/download/v#{version}/anodizer-#{version}.zip"
   homepage 'https://github.com/filippo-liverani/anodizer'
