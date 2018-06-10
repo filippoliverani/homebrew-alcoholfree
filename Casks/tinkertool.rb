c83a34b7cf90a1dd88aef65d4523f6b64c2fdbbc
Filippo Liverani <filippo-liverani@users.noreply.github.com>
2018-06-10 13:43:26 +0200

    Update Tinker Tool cask

--- Casks/tinkertool.rb
+++ Casks/tinkertool.rb
@@ -1,8 +1,8 @@
 cask 'tinkertool' do
-  version '6.1'
-  sha256 '63d704e56ad951fb525ba5baa68c4dfa4e0c6a4e56c9b6c5606d67e57f01a2e3'
+  version '6.3'
+  sha256 '068f3db6830d5cc32f45207ba0a5ef3bca116f84fcb0b02f76fa39230b6ad7e3'
 
-  url 'https://us.softpedia-secure-download.com/dl/4399bb390d1ab11f572fe01e7630e0bc/590f538e/400005270/mac/Utilities/TinkerTool.dmg'
+  url 'https://us.softpedia-secure-download.com/dl/197a0645ee1db100a852e0d142fba5cc/5b1d0dbe/400005270/mac/Utilities/TinkerTool.dmg'
   name 'TinkerTool'
   homepage 'http://www.bresink.com/osx/TinkerTool.html'
 
