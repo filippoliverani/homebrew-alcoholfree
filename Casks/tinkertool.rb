17bf70b72807a056a095d8fc28538cb9646714e9
Filippo Liverani <1382917+filippoliverani@users.noreply.github.com>
2019-10-12 12:27:24 +0200

    Update to 7.41

--- Casks/tinkertool.rb
+++ Casks/tinkertool.rb
@@ -1,8 +1,8 @@
 cask 'tinkertool' do
-  version '6.3'
-  sha256 '068f3db6830d5cc32f45207ba0a5ef3bca116f84fcb0b02f76fa39230b6ad7e3'
+  version '7.41'
+  sha256 '032a2d8a8ea8cc30cb8bb06af5cf9d6dae81ec347f8bc2e9d5b94b3899cc668e'
 
-  url 'https://us.softpedia-secure-download.com/dl/197a0645ee1db100a852e0d142fba5cc/5b1d0dbe/400005270/mac/Utilities/TinkerTool.dmg'
+  url 'https://us.softpedia-secure-download.com/dl/0c1a85a1067174e2912af73d352485ee/5da1a9c9/400005270/mac/Utilities/TinkerTool.dmg'
   name 'TinkerTool'
   homepage 'http://www.bresink.com/osx/TinkerTool.html'
 
